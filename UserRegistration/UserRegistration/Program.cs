4c9202d3858bfe75398f767fd421e3076366f443
Balla <ballaphanindra@outlook.com>
2022-07-13 13:24:15 +0530

    Committing UC1

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -2,11 +2,18 @@
 
 namespace UserRegistration
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FirstName uc1 = new FirstName();
+            bool ucTest1 = uc1.validateFirstName("Shruti");
+            bool ucTest2 = uc1.validateFirstName("12Shruti");
+            bool ucTest3 = uc1.validateFirstName("shruti");
+            Console.WriteLine("{0} {1} {2}", (ucTest1) ? "Valid" : "Invalid", (ucTest2) ? "Valid" : "Invalid", (ucTest3) ? "Valid" : "Invalid");
+          
+
+            Console.ReadKey();
         }
     }
 }
