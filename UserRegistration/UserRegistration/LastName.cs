8071a3a1771a22aa5acd6441c60787cc88002016
Balla <ballaphanindra@outlook.com>
2022-07-13 13:58:24 +0530

    Updating ValidLastName

---
+++ UserRegistration/UserRegistration/LastName.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+	class LastName
+	{
+		// last name should start with a capital alphabet and has minimum 3 characters(including capital alphabet)
+
+		public static string Regex_LastName = "^[A-Z][a-z]{2,}$";
+
+		public bool validateLastName(string lastName)
+		{
+			return Regex.IsMatch(lastName, Regex_LastName);
+		}
+	}
+}
--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -2,11 +2,16 @@
 
 namespace UserRegistration
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LastName uc2 = new LastName();
+            bool uc2Test1 = uc2.validateLastName("Verma");
+            bool uc2Test2 = uc2.validateLastName("12Verma");
+            bool uc2Test3 = uc2.validateLastName("verma");
+            Console.WriteLine("{0} {1} {2}", (uc2Test1) ? "Valid" : "Invalid", (uc2Test2) ? "Valid" : "Invalid", (uc2Test3) ? "Valid" : "Invalid");
+            Console.ReadKey();
         }
     }
 }
