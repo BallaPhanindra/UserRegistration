8de34d56d933e3efd541128f3c551d950bdcc910
Balla <ballaphanindra@outlook.com>
2022-07-13 23:38:08 +0530

    Committing ValidMobileFormat

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -19,6 +19,22 @@ namespace UserRegistration
             bool uc2Test3 = uc2.validateLastName("verma");
             Console.WriteLine("{0} {1} {2}", (uc2Test1) ? "Valid" : "Invalid", (uc2Test2) ? "Valid" : "Invalid", (uc2Test3) ? "Valid" : "Invalid");
             Console.ReadKey();
+
+            #region UC3 Email
+            Email uc3 = new Email();
+            bool uc3Test1 = uc3.validateEmail("abc.xyz@bl.co.in");
+            bool uc3Test2 = uc3.validateEmail("abc.xyz@in");
+            bool uc3Test3 = uc3.validateEmail("abc.@bl.co.in");
+            Console.WriteLine("{0} {1} {2}", (uc3Test1) ? "Valid" : "Invalid", (uc3Test2) ? "Valid" : "Invalid", (uc3Test3) ? "Valid" : "Invalid");
+            #endregion
+
+            #region UC4 Mobile Number
+            MobileNumber uc4 = new MobileNumber();
+            bool uc4Test1 = uc4.validateMobileNumber("91 9876543210");
+            bool uc4Test2 = uc4.validateMobileNumber("12345678");
+            bool uc4Test3 = uc4.validateMobileNumber("123abcd");
+            Console.WriteLine("{0} {1} {2}", (uc4Test1) ? "Valid" : "Invalid", (uc4Test2) ? "Valid" : "Invalid", (uc4Test3) ? "Valid" : "Invalid");
+            #endregion
         }
     }
 }
