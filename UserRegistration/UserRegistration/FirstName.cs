00a8df32d3c27e19b0f5bc575ecb959947911909
Balla <ballaphanindra@outlook.com>
2022-07-13 14:05:39 +0530

    Updating ValidLastName 2nd time

---
+++ UserRegistration/UserRegistration/FirstName.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    internal class FirstName
+    {
+        public static string Regex_FirstName = "^[A-Z][a-z]{2,}$";
+
+        public bool validateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, Regex_FirstName);
+        }
+    }
+}
--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -6,9 +6,16 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
+            FirstName uc1 = new FirstName();
+            bool ucTest1 = uc1.validateFirstName("Balla");
+            bool ucTest2 = uc1.validateFirstName("12Balla");
+            bool ucTest3 = uc1.validateFirstName("Balla");
+            Console.WriteLine("{0} {1} {2}", (ucTest1) ? "Valid" : "Invalid", (ucTest2) ? "Valid" : "Invalid", (ucTest3) ? "Valid" : "Invalid");
+        
+
             LastName uc2 = new LastName();
-            bool uc2Test1 = uc2.validateLastName("Verma");
-            bool uc2Test2 = uc2.validateLastName("12Verma");
+            bool uc2Test1 = uc2.validateLastName("Phani");
+            bool uc2Test2 = uc2.validateLastName("12Phani");
             bool uc2Test3 = uc2.validateLastName("verma");
             Console.WriteLine("{0} {1} {2}", (uc2Test1) ? "Valid" : "Invalid", (uc2Test2) ? "Valid" : "Invalid", (uc2Test3) ? "Valid" : "Invalid");
             Console.ReadKey();
